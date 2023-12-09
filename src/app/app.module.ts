22eafafbc8a78b585eca8abb9cbc1b7c512bc46d
jugiros <jugiros1@gmail.com>
2023-12-09 17:06:26 -0500

    feat: Configuración proveedores

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,8 @@ import {AppBarComponent} from "./components/app-bar/app-bar.component";
 import {ProductViewComponent} from "./views/product-view/product-view.component";
 import {TableComponent} from "./components/table/table.component";
 import {CommonModule} from "@angular/common";
+import {HttpClientModule} from '@angular/common/http';
+import {ProductService} from "./services/product.service";
 
 @NgModule({
   declarations: [
@@ -13,12 +15,17 @@ import {CommonModule} from "@angular/common";
   ],
   bootstrap: [AppComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    HttpClientModule
   ],
   exports: [
     AppBarComponent,
     ProductViewComponent
   ],
-  schemas: [CUSTOM_ELEMENTS_SCHEMA]
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
+  providers: [
+    ProductService
+  ]
 })
-export class AppModule { }
+export class AppModule {
+}
