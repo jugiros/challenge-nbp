031b8a884c0dd1be30086fae51693dd2aa4ec1c3
jugiros <jugiros1@gmail.com>
2023-12-09 17:06:52 -0500

    feat: Visualización de productos

---
+++ src/app/environments/environment.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: false,
+  apiUrl: 'https://tribu-ti-staffing-desarrollo-afangwbmcrhucqfh.z01.azurefd.net/ipf-msa-productosfinancieros'
+};
--- src/app/views/product-view/product-view.component.ts
+++ src/app/views/product-view/product-view.component.ts
@@ -1,22 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { TableData } from '../../components/table/table.component';
+import {ProductoFinanciero} from "../../models/producto-financiero.model";
+import {ProductService} from "../../services/product.service";
 
 @Component({
   selector: 'app-product-view',
   templateUrl: './product-view.component.html',
   styleUrls: ['./product-view.component.css']
 })
-export class ProductViewComponent {
+export class ProductViewComponent implements OnInit {
   tableData: TableData | undefined;
+  productos: ProductoFinanciero[] = [];
 
-  constructor() {
-    this.tableData = {
-      headers: ['Name', 'Age', 'Country'],
-      rows: [
-        ['John Doe', 30, 'USA'],
-        ['Alice Smith', 25, 'Canada'],
-        ['Bob Johnson', 35, 'UK']
-      ]
-    };
+  constructor(private productService: ProductService) {}
+
+  ngOnInit(): void {
+    this.productService.getAll()
+      .subscribe((productos: ProductoFinanciero[]) => {
+        this.productos = productos;
+        this.tableData = this.generateTableData(productos);
+      });
+  }
+
+  generateTableData(productos: ProductoFinanciero[]): TableData {
+    const headers = ['ID', 'Nombre', 'Descripción', 'Logo', 'Fecha de Lanzamiento', 'Fecha de Revisión'];
+    const rows: any[] = [];
+
+    productos.forEach((producto: ProductoFinanciero) => {
+      const row = [
+        producto.id,
+        producto.name,
+        producto.description,
+        producto.logo,
+        producto.date_release,
+        producto.date_revision
+      ];
+      rows.push(row);
+    });
+
+    return { headers, rows };
   }
 }
